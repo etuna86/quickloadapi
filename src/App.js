68cf78abe8653b282bde3c90be82344915445a51
erol tuna <eroltuna86@gmail.com>
2022-04-09 19:22:02 +0300

    request  unlimited done

--- src/App.js
+++ src/App.js
@@ -11,38 +11,34 @@ import axios from 'axios';
 let customHistory = createBrowserHistory();
 
 
+
+
 const baseURL = "https://teknasyon.myshopify.com";
 const AccessToken = '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';
 const org = 'ORG';
 axios.defaults.baseURL = 'https://api.github.com';
 
 //Repository ID, Username, Description, Stars, Forks, Update Date.
-const columns = [
-  {
-    name: 'Repository ID',
-    selector: row => row.id,
+
+/*
+const settings={
+  Username:"etuna86",
+  Password:"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"
+}*/
+var data = JSON.stringify({
+  "Username": "etuna86",
+  "Password": "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"
+});
+
+var config = {
+  method: 'get',
+  url: '/repositories',
+  headers: { 
+    'Authorization': 'Basic ZXR1bmE4NjpnaHBfYWk2YWVTYUc0RVczVEJDU3lsVVNLelNEQ0pZZkxsMGlGZW9l', 
+    'Content-Type': 'application/json'
   },
-  {
-    name: 'Username',
-    selector: row => row.name,
-  },
-  {
-    name: 'Description',
-    selector: row => row.description,
-  },
-  {
-    name: 'Stars',
-    selector:  row => row.name ,
-  },
-  {
-    name: 'Forks',
-    selector: row => row.fork,
-  },
-  {
-    name: 'Update Date',
-    selector: row => row.updatedate,
-  },
-];
+  data : data
+};
 
 
 function App() {
@@ -50,15 +46,41 @@ function App() {
 
   useEffect(() => {
       getRepositories()
-     // getStars();
+     //getStars();
   }, []);
 
-
+  const columns = [
+    {
+      name: 'Repository ID',
+      selector: row => row.id,
+    },
+    {
+      name: 'Username',
+      selector: row => row.owner.login,
+    },
+    {
+      name: 'Description',
+      selector: row => row.description,
+    },
+    {
+      name: 'Stars',
+      selector:    row => getStars(  row.full_name)  ,
+    },
+    {
+      name: 'Forks',
+      selector:  row => row.fork,
+    },
+    {
+      name: 'Update Date',
+      selector: row => row.updatedate,
+    },
+  ];
+  
 
 
   function getRepositories() {
     axios
-      .get(`/repositories`)
+      .get(`/repositories`,config)
       .then((res) => {
         console.warn("res.data.message: ", res.data);
         setRepositories(res.data);
@@ -74,20 +96,17 @@ function App() {
       
   }
 
-  function getStars(getUser){
-    console.warn("getUser: ",getUser);
-    let language;
+  function getStars(fullName){
+    console.warn("getUser: ",fullName);
+    var language="asdfaf";
     axios
-    //.get(`/users/${getUser}/starred`)
-    .get(`/users/etuna/starred`)
+    .get(`repos/${fullName}/languages`,config)
     .then((res) => {
       console.warn("getStars data: ", res.data);
-      language=res.data.language
-      //setRepositories(res.data);
-      //setProductdata(res.data.products);
+      language="ruby";
     }).catch(
       function (error) {
-        console.log('Show error notification!')
+        console.log('Show error notification!123')
         console.warn(error);
         return Promise.reject(error)
       }
